98fae8eb5c34cfb66869e3df73f685ac2c3d1d33
ozywuli <ozy.wu.li@gmail.com>
2015-08-11 16:49:33 -0700

    committed from wrong directory

--- config.rb
+++ config.rb
@@ -24,6 +24,8 @@
 #   page "/admin/*"
 # end
 
+page "/employment/*", :layout => "employment"
+
 # Proxy pages (https://middlemanapp.com/advanced/dynamic_pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
