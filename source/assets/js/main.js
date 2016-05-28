8329ccf34a4878c795a1c6b002a2de60548b16b6
Ozy Wu-Li <ozy.wu.li@gmail.com>
2016-05-28 13:46:03 -0700

    lazy load images

--- source/assets/js/main.js
+++ source/assets/js/main.js
@@ -1,6 +1,14 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
+$(document).ready(function () {
+    navToggleObj.init();
+    lazyLoadImg.init();
+});
+
+// ===============================
+// Toggle nav
+// ===============================
 var navToggleObj = {
     $navToggle: $('.js-topbar-nav-toggle'),
     $nav: $('.nav'),
@@ -16,7 +24,41 @@ var navToggleObj = {
 
 };
 
-$(document).ready(navToggleObj.init());
+// ===============================
+// Lazy Load Images
+// ===============================
+var lazyLoadImg = {
+    $featuredItem: $('.js-featured-list-item'),
+    featuredItemOffsets: [],
+    scrolled: $(window).scrollTop(),
+    windowHeight: $(window).height(),
+
+    updateOffsets: function updateOffsets() {
+
+        var featuredItemOffsets = this.featuredItemOffsets;
+        this.$featuredItem.each(function () {
+            featuredItemOffsets.push($(this).offset().top);
+        });
+    },
+
+    scrolling: function scrolling() {
+        this.scrolled = $(window).scrollTop();
+
+        for (var i = 0; i < this.featuredItemOffsets.length; i++) {
+
+            if (this.scrolled + this.windowHeight - 200 > this.featuredItemOffsets[i]) {
+
+                this.$featuredItem.eq(i).find('.featured-list-item-img').css('background-image', 'url("' + this.$featuredItem.eq(i).attr('data-img') + '")');
+            }
+        }
+    },
+
+    init: function init() {
+        this.updateOffsets();
+
+        $(window).on('scroll', this.scrolling.bind(this));
+    }
+};
 
 },{}]},{},[1])
 
--- tmp/src/assets/js/main.js
+++ tmp/src/assets/js/main.js
@@ -1,3 +1,12 @@
+$(document).ready(function() {
+    navToggleObj.init();
+    lazyLoadImg.init();
+});
+
+
+// ===============================
+// Toggle nav
+// ===============================
 var navToggleObj = {
     $navToggle: $('.js-topbar-nav-toggle'),
     $nav: $('.nav'),
@@ -11,8 +20,41 @@ var navToggleObj = {
         this.$navToggle.on('click', this.toggleNav.bind(this));
     }
 
-
 }
 
+// ===============================
+// Lazy Load Images
+// ===============================
+var lazyLoadImg = {
+    $featuredItem: $('.js-featured-list-item'),
+    featuredItemOffsets: [],
+    scrolled: $(window).scrollTop(),
+    windowHeight: $(window).height(),
 
-$(document).ready(navToggleObj.init());
+    updateOffsets: function() {
+        
+        var featuredItemOffsets = this.featuredItemOffsets;
+        this.$featuredItem.each(function() {
+            featuredItemOffsets.push($(this).offset().top);
+        });
+    },
+
+    scrolling: function() {
+        this.scrolled = $(window).scrollTop();
+
+        for (var i = 0; i < this.featuredItemOffsets.length; i++) {
+
+            if ( (this.scrolled + this.windowHeight - 200) > this.featuredItemOffsets[i] ) {
+
+                this.$featuredItem.eq(i).find('.featured-list-item-img').css('background-image', 'url("' + this.$featuredItem.eq(i).attr('data-img') + '")');
+                
+            }
+        }
+    },
+
+    init: function() {
+        this.updateOffsets();
+
+        $(window).on('scroll', this.scrolling.bind(this));
+    }
+}
